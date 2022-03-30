e1b08bffa04947209a1bee2927861316be30e36f
Rajat Roy <rajatroy.dev@gmail.com>
2022-03-30 21:44:32 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    colors: {
+      'dark': '#0F044C',
+      'light': '#EEEEEE',
+      'primary': '#141E61',
+      'grey': '#787A91'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
